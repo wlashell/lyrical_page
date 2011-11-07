ea08a32b01f10ce28298d778d366ee7b7066271f
Will LaShell <will@lashell.net>
2011-11-06 22:46:45 -0700

    Add tracking code to adming
    Update syntax errors and typos in ga template tag

--- site_tracking/templatetags/site_tracking_tags.py
+++ site_tracking/templatetags/site_tracking_tags.py
@@ -1,6 +1,6 @@
 from django import template
 
-from site_tracking.models import VerificationCode, TRACK_GOOGLE_ANALYTICS
+from site_tracking.models import VerificationCode, TrackingCode, TRACK_GOOGLE_ANALYTICS
 
 register = template.Library()
 
@@ -11,8 +11,8 @@ def st_meta_verification():
 @register.inclusion_tag('site_tracking/js_google_analytics.html')
 def st_ga_tracking():
     try:
-        retval = TrackingCode.objects.get(tracking_code=TRACK_GOOGLE_ANALYTICS).code
+        retval = TrackingCode.objects.get(tracking_type=TRACK_GOOGLE_ANALYTICS).code
     except TrackingCode.DoesNotExist:
         retval = ''
         
-    return {'trackingcode': reval}
+    return {'trackingcode': retval}

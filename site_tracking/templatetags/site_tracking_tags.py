cf90e7d5054a278ec813cae0c7724b34bbd984ef
Will LaShell <will@lashell.net>
2011-11-06 22:34:22 -0700

    adding google analytics site tracking code and a template tag to render it

--- site_tracking/templatetags/site_tracking_tags.py
+++ site_tracking/templatetags/site_tracking_tags.py
@@ -1,9 +1,18 @@
 from django import template
 
-from site_tracking.models import VerificationCode
+from site_tracking.models import VerificationCode, TRACK_GOOGLE_ANALYTICS
 
 register = template.Library()
 
 @register.inclusion_tag('site_tracking/meta_verify.html')
 def st_meta_verification():
     return {'verifycodes': VerificationCode.objects.all()}
+
+@register.inclusion_tag('site_tracking/js_google_analytics.html')
+def st_ga_tracking():
+    try:
+        retval = TrackingCode.objects.get(tracking_code=TRACK_GOOGLE_ANALYTICS).code
+    except TrackingCode.DoesNotExist:
+        retval = ''
+        
+    return {'trackingcode': reval}

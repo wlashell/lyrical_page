54bf33c35d7735b7378252d05b5482e79e38d43c
Chris Ayoub <chriswayoub@gmail.com>
2012-01-06 10:49:56 -0700

    added requires_csrf_token decorator to site page view to allow use of csrf_token template tag in pages served by lyrical page

--- site_content/views.py
+++ site_content/views.py
@@ -4,9 +4,11 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.views.generic.simple import direct_to_template
+from django.views.decorators.csrf import requires_csrf_token
 
 from site_content.models import SitePage, SiteBlock, SiteMenuItem, SiteMenu, SitePageAlias
 
+@requires_csrf_token
 def site_page(request, url, extra_context=None):
     if not url == '/':
         if not url.endswith('/') and settings.APPEND_SLASH:

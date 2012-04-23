c4e730c8d94cb11433739d334f336f6b13a90e5b
Eric Medchill <eric.medchill@gmail.com>
2012-04-22 19:26:51 -0700

    Site tracking updated to work with multiple sites

--- site_tracking/templatetags/site_tracking_tags.py
+++ site_tracking/templatetags/site_tracking_tags.py
@@ -1,17 +1,20 @@
 from django import template
+from django.conf import settings
 
 from site_tracking.models import VerificationCode, TrackingCode, TRACK_GOOGLE_ANALYTICS
 
 register = template.Library()
 
+
 @register.inclusion_tag('site_tracking/meta_verify.html')
 def st_meta_verification():
-    return {'verifycodes': VerificationCode.objects.all()}
+    return {'verifycodes': VerificationCode.objects.filter(site__id=settings.__class__.SITE_ID.value)}
 
+	
 @register.inclusion_tag('site_tracking/js_google_analytics.html')
 def st_ga_tracking():
     try:
-        retval = TrackingCode.objects.get(tracking_type=TRACK_GOOGLE_ANALYTICS).code
+        retval = TrackingCode.objects.get(site__id=settings.__class__.SITE_ID.value, tracking_type=TRACK_GOOGLE_ANALYTICS).code
     except TrackingCode.DoesNotExist:
         retval = ''
         

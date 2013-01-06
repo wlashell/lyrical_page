9832718ccec7bc171c52275f6f8945e6ea757042
Will LaShell <wlashell@lyrical.net>
2013-01-05 22:37:07 -0700

    updates to the setup.py file

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+import os
+
 from setuptools import setup, find_packages
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.txt')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='lyrical_page',
@@ -6,6 +13,8 @@ setup(
     description='Website content system based on a mashup of ideas from the Django contrib flatpage appp, joomla!, and many years of systems development..',
     author='Will LaShell',
     author_email='wlashell@lyrical.net',
+    license='Apache Software License',
+    long_description=README,
     url='http://www.lyrical.net/projects/lyrical_page/',
     packages=find_packages(),
     include_package_data=True,
@@ -18,5 +27,5 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Framework :: Django',
-    ]
+    ],
 )
